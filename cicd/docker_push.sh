e560ee0e4e2dfb6dd742b5ed2a80695c6e1dcdcd
Nick Mitchell <nick@kitset.io>
2019-01-29 20:54:06 +1100

    TravisCI CD

--- cicd/docker_push.sh
+++ cicd/docker_push.sh
@@ -3,4 +3,4 @@ DOCKER_IMAGE=superlogical/tabcorp_client_angular:$TRAVIS_BUILD_NUMBER
 
 docker build -t $DOCKER_IMAGE .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_IMAGE
+docker push $DOCKER_IMAGE 
