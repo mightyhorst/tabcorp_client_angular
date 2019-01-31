311c7cbfc80fdec7a8d1abd0500efb974c33e879
Nick Mitchell <nick@kitset.io>
2019-01-31 22:57:07 +1100

    Updated envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   	production: true,
-  	apiUrl: 'https://l70217a84b.execute-api.ap-southeast-2.amazonaws.com/staging/',
+  	apiUrl: 'http://tabcorp-microservice-book.tabcorp.kitset.io/',
   	searchTimeout: 300,
   	storageStrategy: 'cache'
 };
